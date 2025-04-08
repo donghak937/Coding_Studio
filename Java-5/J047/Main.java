ab0b8f7d450131c2c76f3f79c0d4821c873e0bb8
dongha <donghak937@gmail.com>
2025-04-08 14:22:20 +0900

    add J047

--- Java-5/J047/Main.java
+++ Java-5/J047/Main.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Main pStudio = new Main();
-        pStudio.J046();
+        pStudio.J047();
     }
 
-    void J046() {
+    void J047() {
         Scanner sc = new Scanner(System.in);
         Mystring a = new Mystring();
         String s1 = sc.nextLine();
-        String s2 = sc.nextLine();
+        //String s2 = sc.nextLine();
 
-        System.out.printf(Mystring.whichFirst(s1, s2) + " ");
-        System.out.println(Mystring.whichFirst(s1, s2) == 1 ? s1 : s2);
+        //System.out.printf(Mystring.whichFirst(s1, s2) + " ");
+        //System.out.println(Mystring.whichFirst(s1, s2) == 1 ? s1 : s2);
+        System.out.println(Mystring.removeSpaces(s1));
     }
 
 }
@@ -22,4 +23,8 @@ class Mystring {
     public static int whichFirst(String str1, String str2){
         return str1.compareToIgnoreCase(str2) < 0 ? 1 : 2;
     }
+    public static String removeSpaces(String str){
+        return str.replaceAll(" ",  "");
+    }
 }
+
