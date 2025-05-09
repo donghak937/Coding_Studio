270c8aab384b459562d3ec44ca3375f36cb29b17
dongha <donghak937@gmail.com>
2025-05-09 10:10:42 +0900

    add DS071

---
+++ DS-8/DS071/DS071.cpp
@@ -0,0 +1,45 @@
+// compile for debugging
+// g++ myselectionsort_debug.cpp -DEBUGMODE
+
+//#define DEBUGMODE
+
+#include <iostream>
+using namespace std;
+
+void print(int *arr, int n){
+    for(int i = 0; i < n ; i++)
+        cout << "[" << arr[i] << "] " ;
+    cout << endl;
+}
+
+void selectionSort(int *arr, int n){
+    int min_i = 0;
+
+    for(int i = 0; i < n-1; i++){
+        min_i = i;
+        for(int j = i+1; j < n; j++){
+            if(arr[min_i] > arr[j]) min_i = j;
+        }
+        if(i != min_i) swap(arr[i], arr[min_i]);
+#ifdef DEBUGMODE
+    print(arr, n);
+#endif
+    }
+}
+
+int main(){
+    int *arr;
+    int size;
+    cin >> size;
+    arr = new int[size];
+
+    for(int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+    
+    print(arr, size);
+    selectionSort(arr, size);
+    cout << "====" <<endl;
+    print(arr, size);
+    return 0;
+}
