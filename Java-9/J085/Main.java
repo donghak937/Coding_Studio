458a1dd379dc2f54e2a5a3127594a7a5a81a0370
dongha <donghak937@gmail.com>
2025-05-20 10:57:49 +0900

    add J085

--- Java-9/J085/Main.java
+++ Java-9/J085/Main.java
@@ -8,5 +8,25 @@ public class Main {
     }
     void J085(){
         Scanner input = null;
+        int lower = 0;
+        int upper = 0;
+        int space = 0;
+        try{
+            input = new Scanner(new File("data.txt"));
+            while(input.hasNextLine()){
+                String line = input.nextLine();
+                for(int i = 0 ; i < line.length(); i++){
+                    if (line.charAt(i) >= 'a' && line.charAt(i) <= 'z') lower++;
+                    else if (line.charAt(i) >= 'A' && line.charAt(i) <= 'Z') upper++;
+                    else if (line.charAt(i) == ' ') space++;
+                }
+            }
+            input.close();
+        } catch (FileNotFoundException e){
+            System.out.println("no File");
+        }
+        System.out.println("A~Z - " + upper);
+        System.out.println("a~z - " + lower);
+        System.out.println("Space - " + space);
     }
 }
