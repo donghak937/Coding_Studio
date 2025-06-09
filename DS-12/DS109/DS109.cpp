61cd2a8648e783c5474e62eaa6d709ac355ab188
dongha <donghak937@gmail.com>
2025-06-09 23:03:45 +0900

    add 109

--- DS-12/DS109/DS109.cpp
+++ DS-12/DS109/DS109.cpp
@@ -5,17 +5,23 @@ using namespace std;
 int main(){
     string a;
     stack<char> s;
+
+    stack<char> realWord;
     cin >> a;
     for(int i = 0; i < a.length(); i++){
-        if (a[i] == s.top()){
+        if (!s.empty() && a[i] == s.top()){
             s.pop();
         }
         else {
             s.push(a[i]);
         }
     }
-    while( !s.empty()){
-        cout << s.top();
+    while(!s.empty()){
+        realWord.push(s.top());
         s.pop();
+    }
+    while(!realWord.empty()){
+        cout << realWord.top();
+        realWord.pop();
     }
 }
